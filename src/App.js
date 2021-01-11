53a943a22fcd572cf27d3615adcee3388da2d083
JayDub21 <justin.wofford.21@gmail.com>
2021-01-11 16:00:45 -0600

    linked productDetails

--- src/App.js
+++ src/App.js
@@ -17,7 +17,12 @@ class App extends Component {
         <div className='content'>
           {/* Order routes from most specific to most generic */}
           <Switch>
-            <Route path='/products' component={Products} />
+            <Route path='/products/:id' component={ProductDetails} />
+            <Route
+              path='/products'
+              render={(props) => <Products sortBy='newest' {...props} />}
+            />
+            <Route path='/posts/:year/:month' component={Posts} />
             <Route path='/posts' component={Posts} />
             <Route path='/admin' component={Dashboard} />
             <Route path='/' component={Home} />
